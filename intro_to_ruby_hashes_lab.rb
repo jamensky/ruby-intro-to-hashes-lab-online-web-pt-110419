23a811316a4c254a5fac94538b43741524d85ac2
Jamal Rjoul <jamensky1991@gmail.com>
2019-11-22 23:27:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,9 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {:railroads => {}}
-	monopoly[:railroads] = :pieces
+	monopoly[:railroads][:pieces] = 4
+	monopoly[:railroads][:names] = {}
+	monopoly[:railroads][:rent_in_dollars] = {}
 end
 
 def monopoly_with_third_tier
